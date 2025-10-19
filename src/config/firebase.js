e4352c7d6c90f023f74db5f4b1bc35141a872107
MUHAMMAD RAMZAN <153890643+ramzanbashir@users.noreply.github.com>
2025-10-19 06:56:58 +0500

    nini-hackthon

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDRxX6G-x7ViQS4S8mNz98WIcJ1XV2Bjpg",
-  authDomain: "pitch-craft-93b30.firebaseapp.com",
-  projectId: "pitch-craft-93b30",
-  storageBucket: "pitch-craft-93b30.firebasestorage.app",
-  messagingSenderId: "860200144439",
-  appId: "1:860200144439:web:33f1e5bafce60daca039b2",
-  measurementId: "G-QJWRFK5BFK"
+  apiKey: "AIzaSyDbj0ulEVI6JumN1_XQjgiwtbQrY3Ax0sU",
+  authDomain: "mini--hackthon.firebaseapp.com",
+  projectId: "mini--hackthon",
+  storageBucket: "mini--hackthon.firebasestorage.app",
+  messagingSenderId: "261252327467",
+  appId: "1:261252327467:web:472eebdf152a03d7cda26e",
+  measurementId: "G-1LM3KFYBEB"
 };
 
 const app = initializeApp(firebaseConfig);
